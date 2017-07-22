4d1d3d5fddb6345cb05fd59898d94694fa5aafd6
Hirangi <Hirangi@lenovo>
2017-07-21 23:06:08 -0400

    FIFO ordering

--- src/frontend/services/FIFOOrderSys.java
+++ src/frontend/services/FIFOOrderSys.java
@@ -1,21 +1,39 @@
 package frontend.services;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.LinkedList;
+
 import frontend.entities.Request;
 
-public class FIFOOrderSys 
-{
-	public boolean addRequest(Request newRequest)
-	{
+public class FIFOOrderSys {
+	private LinkedList<Request> queue = new LinkedList<Request>();
+
+	public boolean addRequest(Request newRequest) {
+		queue.add(newRequest);
 		return true;
 	}
-	
-	public String sendFirstRequest(String leadServerHostname, int leadServerPort)
-	{
-		return null;
+
+	public String sendFirstRequest(String leadServerHostname, int leadServerPort) {
+		if (!queue.isEmpty()) {
+			try {
+				DatagramSocket socket = new DatagramSocket();
+				byte[] message = queue.getFirst().toString().getBytes();
+				InetAddress host = InetAddress.getByName(leadServerHostname);
+				DatagramPacket request = new DatagramPacket(message, message.length, host, leadServerPort);
+				socket.send(request);
+
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+			return "Request sent";
+		} else
+			return "Error sending request";
 	}
-	
-	public void removeFirstRequest()
-	{
-		
+
+	public void removeFirstRequest() {
+		queue.removeFirst();
 	}
 }
--- src/replica1/services/impl/FIFOBroadcastSys.java
+++ src/replica1/services/impl/FIFOBroadcastSys.java
@@ -1,11 +1,17 @@
 package replica1.services.impl;
 
+import java.util.List;
+
 import replica1.entities.Request;
 
-public class FIFOBroadcastSys 
-{	
-	public void broadcastRequest(Request newRequest)
-	{
-		//TODO code for broadcasting request to secondary replicas
+public class FIFOBroadcastSys {
+
+	public void broadcastRequest(Request newRequest) {
+		// TODO code for broadcasting request to secondary replicas
+	}
+
+	public void setSecServerDetails(List<String[]> secServerDetails) {
+		
+
 	}
 }
